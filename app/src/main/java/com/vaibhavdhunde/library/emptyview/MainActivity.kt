c387fc0401a0f1c3fa15608bb94bebd6d8ae4727
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2019-09-23 11:56:44 +0530

    Test use empty-view

--- app/src/main/java/com/vaibhavdhunde/library/emptyview/MainActivity.kt
+++ app/src/main/java/com/vaibhavdhunde/library/emptyview/MainActivity.kt
@@ -2,11 +2,16 @@ package com.vaibhavdhunde.library.emptyview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        empty_view.setEmptyViewImage(R.drawable.ic_email)
+        empty_view.setEmptyViewTitle("No mail received yet...")
+        empty_view.setEmptyViewSubtitle("Start sending emails to the contacts...")
     }
 }
